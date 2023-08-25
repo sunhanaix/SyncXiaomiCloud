f5f3f865c6dc8c21adc071f22e6c91d8a48360c0
sunhanaix <58159199+sunhanaix@users.noreply.github.com>
2023-08-26 00:24:03 +0800

    增加device_id的设置，从config.py中读取

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 #配置相关个人信息
+device_id='wb_50894a5e-3ee3-4205-8322-d3e1361a9ef3'  #这个要网页登录小米云，用fiddler抓包或者F12去找到自己的device_id才行
 username='13901238888' #登录的用户名
 password='abc1234' #登录的密码
 do_sha1_first=False #是否强制重新扫描目标目录所有文件，重新计算sha1，建议怀疑本地文件有额外copy进入文件时，将之为True
